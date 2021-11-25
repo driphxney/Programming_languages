717d161731246056adfd9395db0258a9eacfe5ac
Vadim Molotkov <81369917+driphxney@users.noreply.github.com>
2021-11-25 03:17:09 +0300

    Update main.cpp
    2

--- Task 1/Task 1-2/main.cpp
+++ Task 1/Task 1-2/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <cmath>
-/**
-* \brief Главная функция. Точка входа в программу.
-* \return Возвращает 0 в случае успеха.
-* \double .. Обьявляем переменные.
-*/
+
 int main()
 {
-	std::cout << "Введите значения переменных ";
+	std::cout << "Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ РґРІСѓС… РїРµСЂРµРјРµРЅРЅС‹С… a Рё b: ";
 
 	double a = 0;
 	double b = 0;
@@ -19,8 +15,8 @@ int main()
 	double sr_arif = (a + b) / 2;
 	double d = abs(a) * abs(b);
 	double sr_geomtr = sqrt(d);
-	std::cout << "Среднее арифмитическое = " << sr_arif;
-	std::cout << "Среднее геометрическое = " << sr_geomtr;
+	std::cout << "РЎСЂРµРґРЅРµРµ Р°СЂРёС„РјРёС‚РёС‡РµСЃРєРѕРµ = " << sr_arif;
+	std::cout << "РЎСЂРµРґРЅРµРµ РіРµРѕРјРµС‚СЂРёС‡РµСЃРєРѕРµ = " << sr_geomtr;
 
 	return 0;
 }

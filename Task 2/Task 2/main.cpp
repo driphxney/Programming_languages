dfbf01de7dd1f4db9d569fad3bad7fb0d5a51ce4
Vadim Molotkov <81369917+driphxney@users.noreply.github.com>
2021-11-25 13:57:49 +0300

    Update main.cpp

--- Task 2/Task 2/main.cpp
+++ Task 2/Task 2/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 void face_area(){
+	double first = 0;
 	first = a * a;
 	std::cout << "Площадь грани куба = "  << first;
 }
 
 void total_surface_area(){
+	double second = 0;
 	second = first * 6;
 	std::cout << "Площадь полной поверхности куба = " << second;
 }
 
 void volume(){
+	double third = 0;
 	third = a * a * a;
 	std::cout << "Обьем куба = " << third;
 }
@@ -21,10 +24,6 @@ int main()
 	
 	double a = 0;
 	std::cin >> a;
-	
-	double first = 0;
-	double second = 0;
-	double third = 0;
 	
 	face_area();
 	total_surface_area();

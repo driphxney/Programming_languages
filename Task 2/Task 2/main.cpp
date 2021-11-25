6cb59f98f8911515aca4508f5a56f1f2a68fb4ae
Vadim Molotkov <81369917+driphxney@users.noreply.github.com>
2021-11-25 03:47:23 +0300

    Решена задача 2-1

--- Task 2/Task 2/main.cpp
+++ Task 2/Task 2/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+void face_area(){
+	first = a * a;
+	std::cout << "Площадь грани куба = "  << first;
+}
+
+void total_surface_area(){
+	second = first * 6;
+	std::cout << "Площадь полной поверхности куба = " << second;
+}
+
+void volume(){
+	third = a * a * a;
+	std::cout << "Обьем куба = " << third;
+}
+
 int main()
 {
+	std::cout << "Введите значение длины ребра куба: ";
+	
+	double a = 0;
+	std::cin >> a;
+	
+	face_area();
+	total_surface_area();
+	volume();
+		
 	return 0;
 }
